5d368167b9d0c3c036346b153ff3fa9bdefcab1c
zavadskivadzim <kp002210@mail.ru>
2021-10-10 13:17:00 +0300

    task 101

---
+++ src/main/java/com/zavadski/_1_Basics_of_software_code_development/task101.java
@@ -0,0 +1,12 @@
+package com.zavadski._1_Basics_of_software_code_development;
+
+//1. Найдите значение функции: z = ( (a – 3 ) * b / 2) + c.
+public class task101 {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 4;
+        int c = 5;
+        int z = ((a - 3) * b / 2) + c;
+        System.out.println("z=" + z);
+    }
+}
